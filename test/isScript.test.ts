ea45dbe26c49b98c8d4d067cde02b39d5b26dca1
Luca Scalzotto <luca@scorpiac.com>
2023-08-11 10:57:34 +0200

    Add e2e-test and e2e-spec to ignored script infixes

--- test/isScript.test.ts
+++ test/isScript.test.ts
@@ -2,16 +2,20 @@ import { isScript } from '../src/isScript';
 
 describe('isScript', () => {
   it.each`
-    extension            | result
-    ${'file.js'}         | ${true}
-    ${'file.ts'}         | ${true}
-    ${'file.service.ts'} | ${true}
-    ${'file.service.js'} | ${true}
-    ${'file.d.ts'}       | ${false}
-    ${'file.test.ts'}    | ${false}
-    ${'file.test.js'}    | ${false}
-    ${'file.spec.ts'}    | ${false}
-    ${'file.spec.js'}    | ${false}
+    extension             | result
+    ${'file.js'}          | ${true}
+    ${'file.ts'}          | ${true}
+    ${'file.service.ts'}  | ${true}
+    ${'file.service.js'}  | ${true}
+    ${'file.d.ts'}        | ${false}
+    ${'file.test.ts'}     | ${false}
+    ${'file.test.js'}     | ${false}
+    ${'file.spec.ts'}     | ${false}
+    ${'file.spec.js'}     | ${false}
+    ${'file.e2e-test.ts'} | ${false}
+    ${'file.e2e-test.js'} | ${false}
+    ${'file.e2e-spec.ts'} | ${false}
+    ${'file.e2e-spec.js'} | ${false}
   `('returns $result when given $extension', ({ extension, result }) => {
     expect(isScript(extension)).toBe(result);
   });
